0907ff9b12d01cf36b3944aec5f98a8fd9f70f2f
gabriele.lana <gabriele.lana@cleancode.it>
2015-11-22 18:25:12 +0100

    Handle timeout

--- parallel.ex
+++ parallel.ex
@@ -12,23 +12,41 @@ defmodule Waste do
 end
 
 defmodule Parallel do
-  def map(enumerable, f) do
+  def map(enumerable, f, opts \\ []) do
     enumerable
     |> Enum.map(&Task.async(fn -> f.(&1) end))
-    |> collect
+    |> collect(Keyword.get(opts, :timeout, 5_000))
   end
 
-  defp collect(tasks, results \\ [])
+  defp collect(tasks, timeout) do
+    reference = make_ref
+    timer = Process.send_after(self, {:timeout, reference}, timeout)
 
-  defp collect([], results), do: results |> Enum.reverse
-  defp collect(tasks, results) do
+    try do
+      collect(tasks, [], reference)
+    after
+      :erlang.cancel_timer(timer)
+      receive do
+        {:timeout, ^reference} ->
+          :ok
+      after 0 ->
+        :ok
+      end
+    end
+  end
+
+  defp collect([], results, _), do: {:ok, results |> Enum.reverse}
+  defp collect(tasks, results, reference) do
     receive do
+      {:timeout, ^reference} ->
+        {:timeout, length(results), results |> Enum.reverse}
+
       message ->
         case Task.find(tasks, message) do
           {result, task} ->
-            collect(List.delete(tasks, task), [result|results])
+            collect(List.delete(tasks, task), [result|results], reference)
           nil ->
-            collect(tasks, results)
+            collect(tasks, results, reference)
         end
     end
   end
@@ -37,6 +55,6 @@ end
 :random.seed(:os.timestamp)
 
 0..10
-|> Enum.map(fn _ -> :random.uniform(1_000) end)
-|> Parallel.map(&Waste.ms/1)
+|> Enum.map(fn _ -> :random.uniform(2_000) + 1_000 end)
+|> Parallel.map(&Waste.ms/1, timeout: 2_000)
 |> IO.inspect
